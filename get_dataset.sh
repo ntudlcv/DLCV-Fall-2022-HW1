34c24aae5d0e7e59cbb50c6ce3d32e9ba9cae0b9
ntudlcv <72860306+ntudlcv@users.noreply.github.com>
2022-09-14 16:25:32 +0800

    Update get_dataset.sh

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,5 +1,5 @@
 # Download dataset
-gdown 142b4rfsrtHxZ3ym8ioRoDeR78_WeD8dq -O hw1_data.zip
+gdown 1LMIaOY8NSKWmGtbvTsjXcHVaYKnNN_9u -O hw1_data.zip
 
 # Unzip the downloaded zip file
 mkdir hw1_data
